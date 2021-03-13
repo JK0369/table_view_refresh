b4ff477aa944699c65af85ff92107568a9143127
김종권 <jongkwon.kim@42dot.com>
2021-03-13 19:26:29 +0900

    .

--- TableViewEx/ViewController.swift
+++ TableViewEx/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var dataSource = [String]()
     let refreshControl = UIRefreshControl()
     let bag = DisposeBag()
+    @IBOutlet weak var button: UIButton!
 
     lazy var hud: JGProgressHUD = {
         let loader = JGProgressHUD(style: .dark)
@@ -39,6 +40,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         dataSource = ["첫 번째", "두 번째", "세 번째"]
+        for value in 0...50 {
+            dataSource.append(String(describing: value))
+        }
         setUpView()
         setUpInputBinding()
         setUpOutputBinding()
@@ -51,7 +55,7 @@ class ViewController: UIViewController {
 
         // 상단 refresh
         refreshControl.endRefreshing()
-        tableView.refreshControl = refreshControl
+//        tableView.refreshControl = refreshControl
     }
 
     private func setUpInputBinding() {
@@ -63,13 +67,20 @@ class ViewController: UIViewController {
                     self?.refreshLoading.accept(true)
                 }
             }).disposed(by: bag)
+
+        button.rx.tap
+            .bind(onNext: { [weak self] in
+                UIView.animate(withDuration: 1.2) { [weak self] in
+                    self?.tableView.contentOffset.y = 100
+                }
+            }).disposed(by: bag)
     }
 
     private func setUpOutputBinding() {
         // viewModel로부터
-        refreshLoading
-            .bind(to: refreshControl.rx.isRefreshing)
-            .disposed(by: bag)
+//        refreshLoading
+//            .bind(to: refreshControl.rx.isRefreshing)
+//            .disposed(by: bag)
     }
 
 }
@@ -86,14 +97,14 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
-        let currentOffset = scrollView.contentOffset.y
-        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height
-        let deltaOffset = maximumOffset - currentOffset
+        let currentOffset = scrollView.contentOffset.y // frame영역의 origin에 비교했을때의 content view의 현재 origin 위치
+        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height // 화면에는 frame만큼 가득 찰 수 있기때문에 frame의 height를 빼준 것
 
-        if deltaOffset <= 0 {
+        // 스크롤 할 수 있는 영역보다 더 스크롤된 경우 (하단에서 스크롤이 더 된 경우)
+        if maximumOffset < currentOffset {
             // viewModel.loadNextPage()
-            showLoading()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+            showLoading() // 데이터 로딩 중 표시
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
                 self?.hideLoading()
             }
         }
